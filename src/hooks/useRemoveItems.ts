9ebe2db8fb1efaa9a688f6394a8a24616b7ad580
benjuwan <benjuwan.banquet12@gmail.com>
2023-12-15 10:27:42 +0900

    不要なカスタムフックと Context の削除及び main.tsx の調整、一部スタイル微調整.

--- src/hooks/useRemoveItems.ts
+++ src/hooks/useRemoveItems.ts
@@ -11,7 +11,7 @@ export const useRemoveItems = () => {
         const getLocalStorageItems: string | null = localStorage.getItem('localSaveBoxes');
         if (getLocalStorageItems !== null) {
             const SaveDateItems: string[] = JSON.parse(getLocalStorageItems);
-            const filterItems: string[] = SaveDateItems?.filter(isItem => !isItem.match(item));
+            const filterItems: string[] = SaveDateItems?.filter(isItem => !isItem.match(item)); // マッチされないコンテンツデータを返す（例：A, B, C が localStorage データに存在しており、今回 B が削除対象だとするとマッチしないのは A, C なので A, C が（返されて）filterItems となる）
             _localSaved('localSaveBoxes', filterItems);
         }
     }
