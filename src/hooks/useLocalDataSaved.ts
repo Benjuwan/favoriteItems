700aac0d6afea0335cc93c3812f508b1687ea465
benjuwan <benjuwan.banquet12@gmail.com>
2023-12-20 16:43:12 +0900

    useLocalDataSaved カスタムフックの不要な記述を削除.

--- src/hooks/useLocalDataSaved.ts
+++ src/hooks/useLocalDataSaved.ts
@@ -3,7 +3,6 @@ import { LocalStorageContext } from "../provider/LocalStorageContext";
 import { CheckItemsContext } from "../provider/CheckItemsContext";
 import { useLocalSaved } from "./useLocalSaved";
 import { usePushLocalSaveBoxes } from "./usePushLocalSaveBoxes";
-import { useReturnTargetElsStr } from "./useReturnTargetElsStr";
 
 /* localstorage への登録処理 */
 
@@ -13,43 +12,21 @@ export const useLocalDataSaved = () => {
 
     const { _pushLocalSaveBoxes } = usePushLocalSaveBoxes();
     const { _localSaved } = useLocalSaved();
-    const { _returnTargetElsStr } = useReturnTargetElsStr();
 
-    /* 任意の配列から特定のデータ（対象コンテンツのid）を取得して 当該コンテンツの中身（itemContent.innerHTML）を localstorage の配列に格納 */
-    const _getSpecificItems: (targetAry: string[]) => void = (targetAry: string[]) => {
+    const LocalDataSave: () => void = () => {
+        /* 任意の配列から特定のデータ（対象コンテンツのid）を取得して 当該コンテンツの中身（itemContent.innerHTML）を localstorage の配列に格納 */
         const itemContents: NodeListOf<HTMLDivElement> = document.querySelectorAll('.itemsOrigin');
-        targetAry.forEach(targetEl => {
+        isCheckItems.forEach(checkItem => {
             itemContents.forEach(itemContent => {
-                const targetElId = targetEl.split('item-')[1];
+                const checkItemId = checkItem.split('item-')[1];
                 const itemOriginItemId: string | undefined = itemContent.getAttribute('id')?.split('itemsOrigin-')[1];
-                if (targetElId === itemOriginItemId) {
+                if (checkItemId === itemOriginItemId) {
                     const strItemContent: string = String(itemContent.innerHTML);
                     _pushLocalSaveBoxes(strItemContent);
                 }
             });
         });
-    }
-
-    /* 既存の localStorage データの有無を確認して存在する場合は、チェックされているコンテンツの中身（itemsOriginContent）を localstorage の配列に格納及び localstorage を更新する */
-    const LocalDataSave: () => void = () => {
-        const getLocalStorageItems: string | null = localStorage.getItem('localSaveBoxes');
-        if (getLocalStorageItems !== null) {
-            const SaveDateItems: string[] = JSON.parse(getLocalStorageItems);
-            if (SaveDateItems.length > 0) {
-                const checkedItems: NodeListOf<HTMLElement> = document.querySelectorAll('[checked]');
-                if (checkedItems.length > 0) {
-                    const itemsOriginStrs: string[] = _returnTargetElsStr(checkedItems); // 条件に一致する複数要素が持つ「任意の子要素の中身（.itemsOrigin の中身）」を文字列として取得
-                    _pushLocalSaveBoxes(itemsOriginStrs);
-                    _localSaved('localSaveBoxes', localSaveBoxes);
-                }
-            }
-        }
-
-        /* 初回時の処理 */
-        else {
-            _getSpecificItems(isCheckItems);
-            _localSaved('localSaveBoxes', localSaveBoxes);
-        }
+        _localSaved('localSaveBoxes', localSaveBoxes);
     }
 
     return { LocalDataSave }
